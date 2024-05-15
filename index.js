79ef0592631a4befb78f7d178a85036f0f2e1436
Egil Ramsten <egil.ramsten@hotmail.com>
2024-05-15 12:26:32 +0200

    stated to make the folder-tree

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 const express = require('express');
 const sqlite = require('sqlite3');
+
+//modules
+
+// const  = require("");
+
+// const app = express();
+
+// app.use(express.json());
+// app.use("/api", );
+
+const PORT = 9001;
+const URL = "127.0.0.1";
+
+app.listen(PORT, URL, () => {
+  console.log("Bulletin Loaded!");
+});
