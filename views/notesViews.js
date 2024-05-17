eb685a3d66985062e317ffebdc82a6f5c69aa59e
sparrro <116090214+sparrro@users.noreply.github.com>
2024-05-17 11:08:31 +0200

    rewrite of channelnotes, usernotes structure

--- views/notesViews.js
+++ views/notesViews.js
@@ -10,39 +10,43 @@ const db = new sqlite.Database(dbPath, (error) => {
 });
 
 //get view of all notes in a channel
-module.exports.getChannelNotes = (channelId, callback) => {
-    db.run(`
-    CREATE OR REPLACE VIEW channelNotes
-    AS SELECT Channel.channel_ID AS channel,
-    Note.note_ID, Note.user_ID
-    FROM Channel
-    JOIN Note ON Channel.channel_ID = Note.channel_ID
-    WHERE Channel.channel_ID = ?
-    `, [channelId], (error) => {
-        if (error) {
-            console.log(error);
-            callback(error);
-        } else {
-            callback(null);
-        }
-    });
+module.exports.getChannelNotes = (channelId) => {
+    return new Promise((resolve, reject) => {
+        db.run(`
+        CREATE OR REPLACE VIEW channelNotes
+        AS SELECT Channel.channel_ID AS channel,
+        Note.note_ID, Note.user_ID
+        FROM Channel
+        JOIN Note ON Channel.channel_ID = Note.channel_ID
+        WHERE Channel.channel_ID = ?
+        `, [channelId], (error) => {
+            if (error) {
+                console.log(error);
+                reject(error);
+            } else {
+                resolve(``) ; //viewen
+            }
+        });
+    })
 }
 
 //view of all notes by a user
-module.exports.getUserNotes = (userId, callback) => {
-    db.run(`
-    CREATE OR REPLACE VIEW userMessages
-    AS SELECT User.user_ID AS user,
-    Note.note_ID, Note.channel_ID
-    FROM User
-    JOIN Note ON User.user_ID = Note.user_ID
-    WHERE User.user_ID = ?
-    `, [userId], (error) => {
-        if (error) {
-            console.log(error);
-            callback(error);
-        } else {
-            callback(null);
-        }
-    });
+module.exports.getUserNotes = (userId) => {
+    return new Promise((resolve, reject) => {
+        db.run(`
+        CREATE OR REPLACE VIEW userMessages
+        AS SELECT User.user_ID AS user,
+        Note.note_ID, Note.channel_ID
+        FROM User
+        JOIN Note ON User.user_ID = Note.user_ID
+        WHERE User.user_ID = ?
+        `, [userId], (error) => {
+            if (error) {
+                console.log(error);
+                reject(error);
+            } else {
+                resolve(); //viewen
+            }
+        });
+    })
 }
